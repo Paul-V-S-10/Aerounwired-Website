8202f0893b49edabc40d9e670f972bfaf8541ace
Paul-V-S-10 <160314236+Paul-V-S-10@users.noreply.github.com>
2024-12-04 15:00:21 +0530

    Set up initial dependencies and folder structure

---
+++ src/MainRouter.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainUser from "./User/MainUser";
+import HomePage from "./User/Pages/HomePage/HomePage";
+import About from "./User/Pages/About/About";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainUser />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/About",
+          element: <About />
+        }
+      ]
+    },
+    {
+      
+    }
+  
+  ]);
